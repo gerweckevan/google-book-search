5cb6ea8048f90399da43fa35f8cf3aa4476534f5
Evan Gerweck <gerweckevan@gmail.com>
2021-03-07 15:26:16 -0700

    atlas

---
+++ client/build/precache-manifest.77c6111485601dba1204d574cdf69c3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f85d300ba064bd378d17b5e4b04b217",
+    "url": "/index.html"
+  },
+  {
+    "revision": "721a2c3c0bee2e4020b1",
+    "url": "/static/css/main.007cb44d.chunk.css"
+  },
+  {
+    "revision": "abf87b5ce0734232411d",
+    "url": "/static/js/2.40b3acd0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.40b3acd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "721a2c3c0bee2e4020b1",
+    "url": "/static/js/main.4f6eaa73.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  },
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  }
+]);
